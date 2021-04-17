b0b970afc04438492e0e62f2182530f1a3ed2fd6
Alexander <asander@list.ru>
2021-04-17 12:03:42 +1200

    5.1.4. FlatMap для Iterator<Iterator> [#497188]

--- src/main/java/ru/job4j/it/FlatMap.java
+++ src/main/java/ru/job4j/it/FlatMap.java
@@ -8,6 +8,7 @@ import java.util.NoSuchElementException;
 public class FlatMap<T> implements Iterator<T> {
     private final Iterator<Iterator<T>> data;
     private Iterator<T> cursor = Collections.emptyIterator();
+    private int point = 0;
 
     public FlatMap(Iterator<Iterator<T>> data) {
         this.data = data;
@@ -16,6 +17,20 @@ public class FlatMap<T> implements Iterator<T> {
     @Override
     public boolean hasNext() {
 
+        if (cursor == null) {
+            cursor = data.next();
+        }
+
+
+
+        if (cursor.hasNext()) {
+            return true;
+        } else {
+            if (data.hasNext()) {
+                cursor = data.next();
+                return true;
+            }
+        }
         return false;
     }
 
@@ -24,6 +39,7 @@ public class FlatMap<T> implements Iterator<T> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
+
         return cursor.next();
     }
 
