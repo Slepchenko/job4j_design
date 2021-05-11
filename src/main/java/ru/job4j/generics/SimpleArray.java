462954ae01b5ecdbba42cc5e76db4b530f65930f
Alexander <asander@list.ru>
2021-05-11 20:49:14 +1200

    5.2.1. Реализовать SimpleArray<T> [#497200]

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -6,7 +6,6 @@ import java.util.NoSuchElementException;
 public class SimpleArray<T> implements Iterable<T> {
     private int size = 0;
     private T[] array;
-    private int point = 0;
 
     public SimpleArray(int sizeArray) {
         array = (T[]) new Object[sizeArray];
@@ -27,23 +26,26 @@ public class SimpleArray<T> implements Iterable<T> {
         if (!checkIndex(index, size)) {
             throw new IndexOutOfBoundsException();
         }
+            System.arraycopy(array, index + 1, array, index, size - index);
+            array[size-1] = null;
+            size--;
 
-        if (array.length == 1) {
-            array[0] = null;
-            size--;
-            return;
-        }
-        if (size == array.length && size > 1) {
-            for (int i = index; i < size - 1; i++) {
-                array[i] = array[i + 1];
-            }
-            array[size - 1] = null;
-            size--;
-            return;
-        }
-        if (size - index >= 0) System.arraycopy(array, index + 1, array, index, size - index);
-        array[size] = null;
-        size--;
+//        if (array.length == 1) {
+//            array[0] = null;
+//            size--;
+//            return;
+//        }
+//        if (size == array.length && size > 1) {
+//            for (int i = index; i < size - 1; i++) {
+//                array[i] = array[i + 1];
+//            }
+//            array[size - 1] = null;
+//            size--;
+//            return;
+//        }
+//        if (size - index >= 0) System.arraycopy(array, index + 1, array, index, size - index);
+//        array[size] = null;
+//        size--;
     }
 
     public T get(int index) {
@@ -51,15 +53,13 @@ public class SimpleArray<T> implements Iterable<T> {
     }
 
     private boolean checkIndex(int index, int size) {
-        if (index < 0 || index >= size) {
-            return false;
-        }
-        return true;
+        return index >= 0 && index < size;
     }
 
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
+            private int point = 0;
             @Override
             public boolean hasNext() {
                 return point < size;
--- src/test/java/ru/job4j/generics/SimpleArrayTest.java
+++ src/test/java/ru/job4j/generics/SimpleArrayTest.java
@@ -43,7 +43,7 @@ public class SimpleArrayTest {
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
-    public void WhenOverFlowException() {
+    public void whenOverFlowException() {
         SimpleArray<String> test = new SimpleArray<>(2);
         test.add("one");
         test.add("two");
