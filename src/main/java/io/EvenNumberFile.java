20ddd019cb0d2af0d496e640a6353cfdd214cdfd
Alexander <asander@list.ru>
2021-07-22 21:27:24 +1200

    0.2. FileInputStream [#4898]

---
+++ src/main/java/io/EvenNumberFile.java
@@ -0,0 +1,25 @@
+package io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                if (Integer.parseInt(line) % 2 == 0) {
+                    System.out.println("Число " + line + " - четное");
+                } else {
+                    System.out.println("Число " + line + " - нечетное");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
