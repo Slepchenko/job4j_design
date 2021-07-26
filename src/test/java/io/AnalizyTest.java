8c8487e80bb8b9a8c149f956ba8b11097cc66783
Alexander <asander@list.ru>
2021-07-26 18:21:27 +1200

    3.0. Тестирование IO [#173905 #181531]

--- src/test/java/io/AnalizyTest.java
+++ src/test/java/io/AnalizyTest.java
@@ -18,12 +18,12 @@ public class AnalizyTest {
         File source = folder.newFile("source.txt");
         File target = folder.newFile("target.txt");
         try (PrintWriter out = new PrintWriter(source)) {
-            out.println("200 10:56:01" + System.lineSeparator() +
-                    "500 10:57:01" +  System.lineSeparator() +
-                    "400 10:58:01" + System.lineSeparator() +
-                    "200 10:59:01" + System.lineSeparator() +
-                    "500 11:01:02" + System.lineSeparator() +
-                    "200 11:02:02");
+            out.printf("200 10:56:01" + System.lineSeparator());
+            out.printf("500 10:57:01" + System.lineSeparator());
+            out.printf("400 10:58:01" + System.lineSeparator());
+            out.printf("200 10:59:01" + System.lineSeparator());
+            out.printf("500 11:01:02" + System.lineSeparator());
+            out.printf("200 11:02:02");
         }
         Analizy analizy = new Analizy();
         analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
