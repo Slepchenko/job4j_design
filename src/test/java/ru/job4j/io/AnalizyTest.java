f85b0b99dbb83a0fe73aaddb0fcd09e5e1ee35cd
Satrim <59724286+Satrim@users.noreply.github.com>
2021-07-29 22:46:35 +1200

    3.0. Тестирование IO [#173905 #181531]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -19,12 +19,12 @@ public class AnalizyTest {
         File source = folder.newFile("source.txt");
         File target = folder.newFile("target.txt");
         try (PrintWriter out = new PrintWriter(source)) {
-            out.printf("200 10:56:01" + System.lineSeparator());
-            out.printf("500 10:57:01" + System.lineSeparator());
-            out.printf("400 10:58:01" + System.lineSeparator());
-            out.printf("200 10:59:01" + System.lineSeparator());
-            out.printf("500 11:01:02" + System.lineSeparator());
-            out.printf("200 11:02:02");
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
         }
         Analizy analizy = new Analizy();
         analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
