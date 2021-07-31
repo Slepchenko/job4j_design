785cf2762dd5bc8681ca4e16dc41661b5f3fb189
Satrim <59724286+Satrim@users.noreply.github.com>
2021-07-31 16:57:41 +1200

    4.2. Поиск дубликатов [#315066 #182188]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -1,15 +1,30 @@
 package ru.job4j.io.duplicates;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private final Map<FileProperty, Integer> duplicates = new HashMap<>();
+
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
+        File filePath = new File(file.toAbsolutePath().toString());
+        FileProperty fp = new FileProperty(filePath.length(), file.getFileName().toString());
+        Integer value = duplicates.get(fp);
+
+        if (value != null) {
+            duplicates.put(fp, value + 1);
+            System.out.println(fp.getName() + " - " + (value + 1) + " шт.");
+            return super.visitFile(file, attrs);
+        }
+
+        duplicates.put(fp, 0);
         return super.visitFile(file, attrs);
     }
 }
