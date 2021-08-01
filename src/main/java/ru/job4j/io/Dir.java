5a91a936563be605758f8b3babbda48d057abb61
Satrim <59724286+Satrim@users.noreply.github.com>
2021-08-01 16:34:01 +1200

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -1,18 +1,43 @@
 package ru.job4j.io;
 
-import java.io.File;
 
-public class Dir {
-    public static void main(String[] args) {
-        File file = new File("c:\\projects");
+import ru.job4j.io.duplicates.DuplicatesVisitor;
+import ru.job4j.io.duplicates.FileProperty;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Dir extends SimpleFileVisitor<Path> {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
+//        System.out.println(String.format("size : %s", file.getTotalSpace()));
         for (File subfile : file.listFiles()) {
-            System.out.println("File's name: \"" + subfile.getName() + "\", length: " + subfile.length());
+//            System.out.println("File's name: \"" + subfile.getName() + "\", length: " + subfile.length());
+//            if (subfile.getName().endsWith(args[1])) {
+//                System.out.println(subfile.getName());
+//            }
+            System.out.println();
+            String a = subfile.getName();
+            a = a.substring(a.length() - args[1].length());
+            if (a.equals(args[1])) {
+                System.out.println(subfile.getName());
+            }
         }
+
+
     }
 }
