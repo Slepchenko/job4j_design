ec9413ecebacf3d99e7564a030d58fc3cf93a6f2
Satrim <59724286+Satrim@users.noreply.github.com>
2021-08-24 20:36:15 +1200

    6. Кодировка. [#862]

---
+++ src/main/java/ru/job4j/io/UsageEncoding.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.List;
+
+public class UsageEncoding {
+    public String readFile(String path) {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(path, Charset.forName("WINDOWS-1251")))) {
+            br.lines().forEach(builder::append);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+
+    public void writeDataInFile(String path, List<String> data) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("WINDOWS-1251"), true))) {
+            data.forEach(pw::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
