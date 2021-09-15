94b77f15e74f717d90079fcf6250f2376cad1a11
Satrim <59724286+Satrim@users.noreply.github.com>
2021-09-15 17:12:31 +1200

    for surefire

--- src/test/java/ru/job4j/io/ArgsNameTest.java
+++ src/test/java/ru/job4j/io/ArgsNameTest.java
@@ -9,18 +9,24 @@ public class ArgsNameTest {
 
     @Test
     public void whenGetFirst() {
-        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
         assertThat(jvm.get("Xmx"), is("512"));
     }
 
     @Test
     public void whenGetFirstReorder() {
-        ArgsName jvm = ArgsName.of(new String[] {"-encoding=UTF-8", "-Xmx=512"});
+        ArgsName jvm = ArgsName.of(new String[]{"-encoding=UTF-8", "-Xmx=512"});
         assertThat(jvm.get("Xmx"), is("512"));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void whenWrongSomeArgument() {
-        ArgsName jvm = ArgsName.of(new String[] {"-enconding=UTF-8", "-Xmx="});
+        ArgsName jvm = ArgsName.of(new String[]{"-enconding=UTF-8", "-Xmx="});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenGetNotExist() {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx="});
+        jvm.get("Xmx");
     }
 }
