00e8ecff40bcca7373612e6ea328dd32cce1c144
Satrim <59724286+Satrim@users.noreply.github.com>
2021-09-23 13:17:29 +1200

    1. Бот [#7921 #206741]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,19 +3,22 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
+                try (BufferedWriter out = new BufferedWriter(
+                        new OutputStreamWriter(socket.getOutputStream(), Charset.forName("WINDOWS-1251")));
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n");
                     boolean isExit = true;
                     String answer = "What";
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+                    while (in.ready()) {
+                        String str = in.readLine();
                         if (str.contains("Exit")) {
                             isExit = false;
                             answer = "Завершить работу сервера";
@@ -24,7 +27,7 @@ public class EchoServer {
                         }
                         System.out.println(str);
                     }
-                    out.write(answer.getBytes());
+                    out.write(answer);
                     out.flush();
                     if (!isExit) {
                         server.close();
