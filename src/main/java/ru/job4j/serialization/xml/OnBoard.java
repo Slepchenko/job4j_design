75d23b2c976d2e0d7cbfe6f4609014cbd59ced3a
Satrim <59724286+Satrim@users.noreply.github.com>
2021-09-28 17:57:10 +1200

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/xml/OnBoard.java
+++ src/main/java/ru/job4j/serialization/xml/OnBoard.java
@@ -1,10 +1,26 @@
 package ru.job4j.serialization.xml;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "onBoardNum")
 public class OnBoard {
 
+    @XmlAttribute
     private String number;
+
+    public OnBoard() {
+
+    }
 
     public OnBoard(String number) {
         this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "OnBoard{"
+                + "number='" + number + '\''
+                + '}';
     }
 }
--- src/main/java/ru/job4j/serialization/xml/Plane.java
+++ src/main/java/ru/job4j/serialization/xml/Plane.java
@@ -1,12 +1,28 @@
 package ru.job4j.serialization.xml;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Arrays;
 
+@XmlRootElement(name = "plane")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Plane {
-    private final boolean warplane;
-    private final int age;
-    private final OnBoard onBoardNum;
-    private final String[] data;
+
+    @XmlAttribute
+    private boolean warplane;
+
+    @XmlAttribute
+    private int age;
+    private OnBoard onBoardNum;
+    private String[] data;
+
+    public Plane() {
+    }
 
     public Plane(boolean warplane, int age, OnBoard onBoardNum, String[] data) {
         this.warplane = warplane;
@@ -25,8 +41,27 @@ public class Plane {
                 + '}';
     }
 
-    public static void main(String[] args) {
-        Plane plane1 = new Plane(false, 5, new OnBoard("VQ-BDU"), new String[]{"Passenger", "Russian"});
-        System.out.println(plane1);
+    public static void main(String[] args) throws JAXBException {
+        final Plane plane = new Plane(false, 5, new OnBoard("VQ-BDU"),
+                new String[]{"Passenger", "Russian"});
+        String result = "";
+        JAXBContext context = JAXBContext.newInstance(Plane.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(plane, writer);
+            result = writer.getBuffer().toString();
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(result)) {
+            Plane res = (Plane) unmarshaller.unmarshal(reader);
+            System.out.println(res);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
