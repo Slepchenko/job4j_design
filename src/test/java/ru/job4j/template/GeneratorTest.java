180f226929c27c5cc74a393641431e1e169e58df
Satrim <59724286+Satrim@users.noreply.github.com>
2022-05-19 18:22:27 +1200

    3. Шаблонизатор. [#855 #302197]

--- src/test/java/ru/job4j/template/GeneratorTest.java
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.template;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -8,6 +10,17 @@ import java.util.Map;
 
 @Ignore
 public class GeneratorTest {
+
+    @Test
+    public void when() {
+        Generator generator = new TemplateGenerator();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Alexander");
+        args.put("subject", "you");
+        assertThat(generator.produce(template, args),
+                is("I am a Alexander, Who are you?"));
+    }
 
     @Test(expected = IllegalArgumentException.class)
     public void whenNoKeyInPattern() {
