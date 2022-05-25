467b9192afad5709e1e81c51841b28a473f4f13d
Satrim <59724286+Satrim@users.noreply.github.com>
2022-05-25 17:50:03 +1200

    0. Принцип единственной ответственности [#4913]

--- src/main/java/ru/job4j/srp/Srp.java
+++ src/main/java/ru/job4j/srp/Srp.java
@@ -5,10 +5,8 @@ public class Srp {
     /**
      * 1-й класс делает кофе и продает напитки, нарушает принцип SRP, нужно
      * разделить их на два класса
-     * 2-й класс зависит от реализации, я так понял, нужно делать отдельные
-     * интерфейсы на каждый метод потом их реализовывать один или несколько
-     * 3-й создает и инициализирует объект. Если я правильно понял, нужно
-     * через конструктор инициализировать реалезацию makeCoffee
+     * Паттерн синглтон нарушает srp. Проверяет наличие ранее созданных экземпляров
+     *
      */
 
     class CoffeeMachine1 {
@@ -19,34 +17,10 @@ public class Srp {
         public void makeCoffee() {
             System.out.println("Варит кофе");
         }
-    }
 
-    class MakeCoffee {
-        public String cappuccino() {
-           return "капучино";
-        }
-
-        public String americano() {
-            return "американо";
-        }
 
     }
 
-    class CoffeeMachine3 {
 
-        MakeCoffee makeCoffee = new MakeCoffee();
-
-        public String make() {
-            return makeCoffee.americano();
-        }
-
-        public int pay() {
-            if (make().equals("американо")) {
-                return 100;
-            } else {
-                return 150;
-            }
-        }
-    }
 
 }
