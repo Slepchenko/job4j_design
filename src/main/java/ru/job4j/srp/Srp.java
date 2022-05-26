31bd8c9dfb26458f9d4eb00f84bd52ddabdc4dca
Satrim <59724286+Satrim@users.noreply.github.com>
2022-05-26 17:21:15 +1200

    0. Принцип единственной ответственности [#4913 #304515]

--- src/main/java/ru/job4j/srp/Srp.java
+++ src/main/java/ru/job4j/srp/Srp.java
@@ -5,8 +5,8 @@ public class Srp {
     /**
      * 1-й класс делает кофе и продает напитки, нарушает принцип SRP, нужно
      * разделить их на два класса
-     * Паттерн синглтон нарушает srp. Проверяет наличие ранее созданных экземпляров
-     *
+     * 2. Паттерн синглтон
+     * 3. метод print нарушает принцип srp
      */
 
     class CoffeeMachine1 {
@@ -17,10 +17,32 @@ public class Srp {
         public void makeCoffee() {
             System.out.println("Варит кофе");
         }
+    }
 
-
+    private static Srp instance = null;
+    public static Srp getInstance() {
+        if (instance == null) {
+            instance = new Srp();
+        }
+        return instance;
     }
 
 
+    class User {
+
+        String name;
+
+        public void print() {
+            System.out.println(name);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 
 }
