ca5e49d76fcb4abce5f739cfeaabc1245aee18b9
Satrim <59724286+Satrim@users.noreply.github.com>
2022-05-27 18:12:45 +1200

    1. Отчеты. [#850 #305327]

---
+++ src/test/java/ru/job4j/srp/ReportEngineTest.java
@@ -0,0 +1,69 @@
+package ru.job4j.srp;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+import java.util.Calendar;
+
+public class ReportEngineTest {
+
+    @Test
+    public void whenOldGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        Report engine = new ReportEngine(store);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(worker.getName()).append(";")
+                .append(worker.getHired()).append(";")
+                .append(worker.getFired()).append(";")
+                .append(worker.getSalary()).append(";")
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true), is(expect.toString()));
+    }
+
+    @Test
+    public void whenAccountingReport() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker1 = new Employee("Ivan", now, now, 100);
+        store.add(worker1);
+        Employee worker2 = new Employee("Vitia", now, now, 90);
+        store.add(worker2);
+        Employee worker3 = new Employee("Vitalya", now, now, 110);
+        store.add(worker3);
+        Report engine = new AccountingReport(store);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(worker3.getName()).append(";")
+                .append(worker3.getSalary()).append("$;")
+                .append(System.lineSeparator())
+                .append(worker1.getName()).append(";")
+                .append(worker1.getSalary()).append("$;")
+                .append(System.lineSeparator())
+                .append(worker2.getName()).append(";")
+                .append(worker2.getSalary()).append("$;")
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true), is(expect.toString()));
+
+    }
+
+    @Test
+    public void whenHrReport() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        Report engine = new HrReport(store);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Salary;")
+                .append(System.lineSeparator())
+                .append(worker.getName()).append(";")
+                .append(worker.getSalary()).append(";")
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true), is(expect.toString()));
+    }
+}
