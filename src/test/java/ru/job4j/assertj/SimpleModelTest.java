5758eedc0e41360d72da195ab6f666e2b03a6f2b
Satrim <59724286+Satrim@users.noreply.github.com>
2022-09-01 01:17:28 +1200

    Утверждения с исключениями [#504886]

--- src/test/java/ru/job4j/assertj/SimpleModelTest.java
+++ src/test/java/ru/job4j/assertj/SimpleModelTest.java
@@ -25,9 +25,7 @@ class SimpleModelTest {
         String word = "name";
         int number = 5;
         assertThatThrownBy(() -> sm.setName(word, number))
-                /*проверяем класс исключения: */
                 .isInstanceOf(IllegalArgumentException.class)
-                /*проверяем факт наличия сообщения*/
                 .message()
                 .isNotEmpty();
     }
@@ -38,13 +36,9 @@ class SimpleModelTest {
         String word = "name";
         int number = 5;
         assertThatThrownBy(() -> sm.setName(word, number))
-                /*проверяем класс исключения: */
                 .isInstanceOf(IllegalArgumentException.class)
-                /*с помощью регулярного выражения проверяем факт наличия сообщения*/
                 .hasMessageMatching("^.+")
-                /*проверяем, что в сообщении есть соответствующие параметры:*/
                 .hasMessageContaining(word, number)
-                /*проверяем наличие конкретного слова в сообщении:*/
                 .hasMessageContaining("name");
     }
 }
