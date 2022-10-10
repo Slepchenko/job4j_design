85ba57c43a316b93b96fb7f57257b453a366f9a8
Satrim <59724286+Satrim@users.noreply.github.com>
2022-10-11 03:57:43 +1200

    1. Динамическое перераспределение продуктов [#854 #350080]

--- src/main/java/ru/job4j/dip/store/foods/Sausage.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.dip.store.foods;
-
-import java.util.Calendar;
-
-public class Sausage extends Food {
-    public Sausage(String name, Calendar expiryDate, Calendar createDate, double price, int discount) {
-        super(name, expiryDate, createDate, price, discount);
-    }
-}
